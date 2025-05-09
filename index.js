a883588c3034d6ff7dd4eb92b8651d2441d3b289
Anonymous Hacker <suprodipbhattacharjee1@gmail.com>
2025-05-09 23:33:29 +0530

    next

--- index.js
+++ index.js
@@ -1,60 +1,10 @@
 import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import multer from "multer";
-
-// import { connectDb } from "./blogServer/connectDb.js";
-import authRoutes from "./routes/auth.routes.js";
-
-import blogRoutes from "./routes/blog.routes.js";
-
-import { verifyToken } from "./middleware/verifyToken.js";
-
-import { connectDb } from "./connectDb.js";
-
-dotenv.config();
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://blogging-frontend-blue.vercel.app", // replace with your actual frontend URL
-    credentials: true, // this allows cookies to be sent/received
-  })
-);
-
-app.options("*", cors());
-
-//connection with db
-connectDb(process.env.MONGO_URI)
-  .then(function () {
-    console.log("connected with db");
-  })
-  .catch(function (err) {
-    console.log("not connected with db", err);
-  });
-
-//middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-//routes
-
-app.get("/test", function (req, res) {
-  res.status(200).json({
-    msg: "working successfully",
-  });
+app.get("/test", (req, res) => {
+  res.send("Hello from minimal server");
 });
 
-app.use("/api/auth", authRoutes);
-
-app.use("/api/blog", verifyToken, blogRoutes);
-
-try {
-  app.listen(process.env.PORT, () => {
-    console.log("Listening on port", process.env.PORT);
-  });
-} catch (err) {
-  console.error("App failed to start due to:", err);
-}
+app.listen(process.env.PORT, () => {
+  console.log("Minimal server running on port 3000");
+});
